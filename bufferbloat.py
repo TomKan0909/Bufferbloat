5be528888a33368e85b9a3753ca8de92f2ff3f6c
TomKan <tomkan0909@gmail.com>
2022-11-23 20:56:51 -0500

    XD

--- bufferbloat.py
+++ bufferbloat.py
@@ -206,7 +206,7 @@ def bufferbloat():
         h2 = net.get('h2')
         for _ in range(3):
             # time_fetch.append(h2.popen("curl -o /dev/null -s -w %%{time_total} %s/http/index.html > %s/curl.txt"%(h1.IP(), args.dir), shell=True).communicate()[0])
-            time_fetch.append(h2.popen("curl -o /dev/null -s -w %%{time_total} %s/http/index.html"%(h1.IP()), shell=True).communicate()[0])
+            time_fetch.append(float(h2.popen("curl -o /dev/null -s -w %%{time_total} %s/http/index.html"%(h1.IP()), shell=True)).communicate()[0])
         sleep(5) # every 5 seconds
 
     # TODO: compute average (and standard deviation) of the fetch
